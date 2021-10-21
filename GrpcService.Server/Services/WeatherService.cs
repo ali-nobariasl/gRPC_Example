d91b9a2b01f0e2aad5415e3ab761c0cea032aa1f
aliko <freeeeboy.gh@gmail.com>
2021-10-21 11:22:20 +0200

    FirstCommit

---
+++ GrpcService.Server/Services/WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using GrpcService.Server.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static GrpcService.Server.Protos.WeatherService;
+
+namespace GrpcService.Server.Services
+{
+    public class WeatherService : WeatherServiceBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public WeatherService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public override Task<WeatherResponse> GetCurrentWeather(
+            GetCurrentWeatherForCityRequest request, ServerCallContext context)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var responseText= httpClient.GetStringAsync(
+                $"http://api.openweathermap.org/data/2.5/weather?q={request.City}&appid=07502ba820841f05edd5b80940f9527d&units={request.Units}") ;
+            return null;
+        }
+    }
+}
